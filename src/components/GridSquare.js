40b0e6e78dd61eaf1ff673bb61970435f4ab471a
August Reid <c.august.reid@gmail.com>
2023-02-16 12:57:22 -0700

    Create GridSquare component

---
+++ src/components/GridSquare.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//represents a grid square with a color
+
+export default function GridSquare(props) {
+  const classes = `grid-square color-${props.color}`;
+
+  return(
+    <div className={classes}/>
+  )
+};
